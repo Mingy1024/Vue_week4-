3bfabcb7ab2f384d8eb3257aac448d2d1d6ccd8a
Mingy1024 <s2142717@gmail.com>
2022-02-27 23:12:36 +0800

    新增加入&刪除購物車功能

--- cart.js
+++ cart.js
@@ -9,6 +9,7 @@ const app = createApp({
       products: [],
       cartData: {},
       productId: "",
+      isLoadingItem:'',
     };
   },
   methods: {
@@ -23,9 +24,55 @@ const app = createApp({
       this.productId = id;
       this.$refs.productModal.openModal();
     },
+    getCart() {
+      axios.get(`${apiUrl}/api/${apiPath}/cart`).then((res) => {
+        console.log(res);
+        this.cartData = res.data.data;
+      });
+    },
+    addToCart(id,qty = 1) {
+      const data = {
+        product_id : id,
+        qty,
+      };
+      this.isLoadingItem = id;
+      axios.post(`${apiUrl}/api/${apiPath}/cart`,{ data }).then((res) => {
+        console.log(res);
+        this.getCart();
+        this.$refs.productModal.closeModal();
+        this.isLoadingItem = '';
+      });
+    },
+    removeCartItem(id) {
+      this.isLoadingItem = id;
+      axios.delete(`${apiUrl}/api/${apiPath}/cart/${id}`).then((res) => {
+        this.getCart();
+        this.isLoadingItem = '';
+      });
+    },
+    removeEntireCart(){
+      this.isLoadingItem = true;
+      axios.delete(`${apiUrl}/api/${apiPath}/carts`).then((res) => {
+        console.log(res);
+        this.getCart();
+        this.isLoadingItem = '';
+      });
+    },
+    updateCartItem(item) {
+      const data = {
+        product_id : item.id,
+        qty: item.qty,
+      };
+      this.isLoadingItem = item.id;
+      axios.put(`${apiUrl}/api/${apiPath}/cart/${item.id}`,{ data }).then((res) => {
+        this.getCart();
+        this.isLoadingItem = '';
+      });
+    }
   },
   mounted() {
     this.getProduct();
+    this.getCart();
   },
 });
 
@@ -36,6 +83,7 @@ app.component("product-modal", {
     return {
       myModal: {},
       product: {},
+      qty: 1,
     };
   },
   watch: {
@@ -47,15 +95,21 @@ app.component("product-modal", {
     openModal() {
       this.myModal.show();
     },
+    closeModal() {
+      this.myModal.hide();
+    },
     getProduct() {
       axios.get(`${apiUrl}/api/${apiPath}/product/${this.id}`)
       .then((res) => {
         this.product = res.data.product;
       });
     },
+    addToCart() {
+      this.$emit('add-cart',this.product.id,this.qty);
+    }
   },
   mounted() {
-    this.myModal = new bootstrap.Modal(this.$refs.modal);
+    this.myModal = new bootstrap.Modal(this.$refs.modal,{ backdrop :'static' });
   },
 });
 
