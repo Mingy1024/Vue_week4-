da1f341fb862a09b27d1ff4e3716abf3a216aa6a
Mingy1024 <s2142717@gmail.com>
2022-03-01 21:13:20 +0800

    將Vue改用CDN載入

--- cart.js
+++ cart.js
@@ -1,10 +1,9 @@
-import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.31/vue.esm-browser.min.js";
 import pagination from "./pagination.js";
 
 const apiUrl = "https://vue3-course-api.hexschool.io/v2";
 const apiPath = "mingyo";
 
-const app = createApp({
+const app = Vue.createApp({
   components:{
     pagination
   },
